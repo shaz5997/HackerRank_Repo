61aef5ebac3af0dec4362b61e6c79d98f0a0de5a
Shaziya Fatma <shaziya.f@techaspect.com>
2018-09-26 19:12:05 +0530

    added source code for the challenge Print reverse

---
+++ Day_5/PrintReverse.js
@@ -0,0 +1,96 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep) {
+    while (node != null) {
+        process.stdout.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            process.stdout.write(sep);
+        }
+    }
+}
+
+function reversePrint(head) {
+    var count=0;
+         var a = [];
+        
+        while(head !=null){
+            a[count]=head.data;
+            count++;
+            head = head.next;
+        }
+        
+        var length = a.length;
+        
+        for(var i=count-1;i>=0;i--){
+            console.log(a[i]);
+        }
+
+}
+
+function main() {
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new SinglyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        reversePrint(llist.head);
+    }
+}
