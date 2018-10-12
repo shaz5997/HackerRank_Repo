f4fde2b040b32eb6a10a20c9bed7b9a41c3115a9
Shaziya Fatma <shaziya.f@tadigital.com>
2018-10-12 14:15:36 +0530

    Added source code for the challenge InsertionSortI in JS

---
+++ Day_15/InsertionSortI.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the insertionSort1 function below.
+function insertionSort1(n, arr) {
+
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+            console.log(...arr);
+        }
+        arr[j + 1] = key;
+    }
+    console.log(...arr);
+
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    insertionSort1(n, arr);
+}
