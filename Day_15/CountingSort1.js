fde52f7ff2369df097d7e97914a09eb744a1a668
Shaziya Fatma <shaziya.f@tadigital.com>
2018-10-12 17:03:04 +0530

    added source code for the challenge CountingSort in JS

---
+++ Day_15/CountingSort1.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingSort function below.
+function countingSort(arr) {
+    var array = new Array(100);
+    for (var i = 0; i < 100; i++) {
+        array[i] = 0;
+    }
+    for (var i in arr) {
+        array[arr[i]] = array[arr[i]] + 1;
+    }
+    return array;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = countingSort(arr);
+
+    ws.write(result.join(" ") + "\n");
+
+    ws.end();
+}
