f5d9217e4a60735ab4013da7ca43be96028d6724
Shaziya Fatma <shaziya.f@tadigital.com>
2018-10-15 16:02:37 +0530

    added source code for the challenge Pairs solution in JS

---
+++ Day_16/Pairs.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the pairs function below.
+
+function pairs(k, arr) {
+    arr.sort(function(a,b) {
+        return a-b;
+    });
+    var i=0,j=1,count=0;
+    var n=arr.length;
+    while(j < n) {
+        var diff = arr[j] - arr[i];
+        
+        if (diff == k) {
+            count++;
+            j++;
+        } else if (diff > k) {
+            i++;
+        } else if (diff < k) {
+            j++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = pairs(k, arr);
+
+    ws.write(result + "\n");
+    
+
+    ws.end();
+}
