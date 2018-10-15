2ef63d1f83293e0b7d9c5aed880b330c39f49e3b
Shaziya Fatma <shaziya.f@tadigital.com>
2018-10-15 15:20:28 +0530

    added source code for the challenge CircularArrayRotation solution in JS

---
+++ Day_11/CircularArrayRotation.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the circularArrayRotation function below.
+function circularArrayRotation(a, k, queries) {
+var array=[];
+    k=k%a.length;
+    for(var i=a.length-k;i<a.length;i++) {
+        array.push(a[i]);
+    }
+    for(var i=0;i<a.length-k;i++) {
+        array.push(a[i]);
+    }
+    var result=[];
+    for(var i in queries) {
+        result.push(array[queries[i]]);
+    }
+    console.log(array);
+    return result;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nkq = readLine().split(' ');
+
+    const n = parseInt(nkq[0], 10);
+
+    const k = parseInt(nkq[1], 10);
+
+    const q = parseInt(nkq[2], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    let queries = [];
+
+    for (let i = 0; i < q; i++) {
+        const queriesItem = parseInt(readLine(), 10);
+        queries.push(queriesItem);
+    }
+
+    const result = circularArrayRotation(a, k, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
